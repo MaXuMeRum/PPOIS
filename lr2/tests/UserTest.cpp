7e70211f894411c3903448e3159f54f45d8d02bf
MaXuMeRum <144938023+MaXuMeRum@users.noreply.github.com>
2024-12-21 01:45:19 +0300

    Delete lr2/tests directory

--- lr2/tests/UserTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "User.h"
-#include <gtest/gtest.h>
-
-TEST(UserTest, ValidateCredentials) {
-    User user("admin", "password", "admin");
-
-    EXPECT_TRUE(user.validateCredentials("admin", "password"));
-    EXPECT_FALSE(user.validateCredentials("admin", "wrong_password"));
-}
