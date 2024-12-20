2a4c5ccecdafbd90ff5f56e34239c0f3d029c019
MaXuMeRum <144938023+MaXuMeRum@users.noreply.github.com>
2024-12-21 00:25:38 +0300

    Delete lr2 directory

--- lr2/PaymentTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "Payment.h"
-#include <gtest/gtest.h>
-
-TEST(PaymentTest, ValidatePayment) {
-    Payment payment(1, 1500.0, time(nullptr), "Кредитная карта");
-
-    EXPECT_TRUE(payment.validatePayment());
-    EXPECT_EQ(payment.getAmount(), 1500.0);
-}
--- lr2/PromotionTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "Promotion.h"
-#include <gtest/gtest.h>
-
-TEST(PromotionTest, CheckValidity) {
-    Promotion promotion(1, "Скидка на выходные", "20% скидка", time(nullptr), time(nullptr) + 86400);
-
-    EXPECT_TRUE(promotion.isActive());
-    EXPECT_NO_THROW(promotion.displayPromotionInfo());
-}
